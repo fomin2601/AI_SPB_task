7087c4727cd782327052dd2f865933d996ed95c4
python_dikrylosov <48491372+python-dikrylosov@users.noreply.github.com>
2023-08-19 12:24:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ name = None
 row_status = None
 import numpy as np
 import pandas as pd
+import matpplotlib.pyplot as plt
 import time
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
@@ -10,4 +11,18 @@ speak.Rate = 3
 speak.Volume = 100
 
 text = "бот активирован"
-speak.Speak(text)
+
+def read_speak(text):
+    speak.Speak(text)
+
+while True:
+    read_speak(text)
+    read_speak("создаю временные ряды")
+    for i in range(10):
+        time.sleep(1)
+        data_time = str(time.time())
+        print(np.array([i,data_time]))
+        if i == 10:
+            print(i)
+            read_speak("закрываю программу")
+            exit()
